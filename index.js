6f87f9924dc60ecf48aa55ec20784bfbd5cb9959
vijay <vijay.kumarp@yahoo.com>
2025-07-08 18:14:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+Add a simple index.js:
+
+js
+Copy
+Edit
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("CI/CD working!"));
+
+app.listen(3000, () => console.log("App running on port 3000"));
+
+
